b4a2ad691f34e7c1d60a51c63f41237404670310
MkySS <62342977+MkySS@users.noreply.github.com>
2020-11-24 22:53:35 +0000

    Criar tabela produtos da base de dados

---
+++ GuardaPelaCultura/Data/Migrations/20201124224955_produtosMigration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GuardaPelaCultura.Data.Migrations
+{
+    public partial class produtosMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Produtos",
+                columns: table => new
+                {
+                    ProdutosId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NomeProduto = table.Column<string>(nullable: true),
+                    DescricaoProduto = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produtos", x => x.ProdutosId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Produtos");
+        }
+    }
+}
