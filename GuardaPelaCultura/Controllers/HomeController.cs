53cf187523a8e3f2c564bd36abcbb782aaa726df
Gonçalo Saraiva <goncalosaraiva00@hotmail.com>
2020-11-26 10:42:53 +0000

    Adicionada página de Registo para a reserva da mesa

--- GuardaPelaCultura/Controllers/HomeController.cs
+++ GuardaPelaCultura/Controllers/HomeController.cs
@@ -30,6 +30,10 @@ namespace GuardaPelaCultura.Controllers
         {
             return View();
         }
+        public IActionResult Registo()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
