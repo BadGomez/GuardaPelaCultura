a57f690ac0fa0e175954c608f1d666f9030faf0d
Gonçalo Saraiva <goncalosaraiva00@hotmail.com>
2020-11-26 11:18:36 +0000

    Adicionado um campo da tabela takeaway mais validações,(ainda não está feita a migração)

--- GuardaPelaCultura/Models/ReservasTakeAway.cs
+++ GuardaPelaCultura/Models/ReservasTakeAway.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace GuardaPelaCultura.Models
 {
@@ -9,8 +10,14 @@ namespace GuardaPelaCultura.Models
     {
         public int ReservasTakeAwayId { get; set; }
 
+
         public string NomeRestaurante { get; set; }
 
+        [Required(ErrorMessage = "Escreva o seu nome por favor")]
+        [StringLength(80, MinimumLength = 2, ErrorMessage = "O nome tem de ter no mínimo 2 caracteres e no máximo 80")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Escreva o seu número de telefone por favor")]
         public string NumeroTelefone { get; set; }
 
         public string Descricao { get; set; }
