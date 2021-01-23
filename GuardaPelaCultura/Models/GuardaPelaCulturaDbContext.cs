64d6c12528145107cd001e42d6e88ede336f2890
Gonçalo Saraiva <goncalosaraiva00@hotmail.com>
2021-01-23 17:12:45 +0000

    SeedData Incompleto

---
+++ GuardaPelaCultura/Models/GuardaPelaCulturaDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GuardaPelaCultura.Models
+{
+    public class GuardaPelaCulturaDbContext : DbContext
+    {
+        public GuardaPelaCulturaDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet <Restaurantes> Restaurantes { get; set; }
+    }
+}
