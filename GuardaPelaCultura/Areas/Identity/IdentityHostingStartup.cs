ccd7f14fbcf89e7b51c0f9e3e01a5300f7a3b52b
Valter Simões <valterrsimoes@gmail.com>
2021-01-26 23:04:12 +0000

    Login Finalmente Concluido

---
+++ GuardaPelaCultura/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using GuardaPelaCultura.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(GuardaPelaCultura.Areas.Identity.IdentityHostingStartup))]
+namespace GuardaPelaCultura.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
